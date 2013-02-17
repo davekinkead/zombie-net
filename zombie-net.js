3ccfbef3c32c62c51e9138ffc0ae45f7dd4141d2
Ben Davies <brdavies@gmail.com>
2013-02-17 18:26:52 +1000

    The UDP datagram is now JSON.

--- zombie-net.js
+++ zombie-net.js
@@ -1,99 +1,126 @@
 var zombieNet = (function(){
 
-	// 	Packages
-	var _ = require('underscore');
-	var Mustache = require('mustache')
-	var fs = require('fs')
-	var dgram = require('dgram');
-	var http = require('http');
-	var os = require('os')
+  // 	Packages
+  var _ = require('underscore');
+  var Mustache = require('mustache')
+  var fs = require('fs')
+  var dgram = require('dgram');
+  var http = require('http');
+  var os = require('os')
 
-	//	Important zombie variables
-	var ips = [];
-	var host = '255.255.255.255';
-	var port = '27246';
-	var adjectives = ['Moaning', 'Hungry', 'Decomposing', 'Green', 'Deathly', 'Evil', 'Nasty', 'Brainless', 'Crazy', 'Horrible', 'Sloppy'];
-	var names = ['Bob', 'Katey', 'Harry', 'Pete', 'Dave', 'Sam', 'Eugene', 'Sally', 'Geoff', 'Nigel', 'Anton', 'Ben'];
-	var zombieName = adjectives[_.random(adjectives.length)] + ' ' + names[_.random(names.length)];
+  //	Important zombie variables
+  var ips = [];
+  var host = '255.255.255.255';
+  var port = '27246';
+  var adjectives = ['Moaning', 'Hungry', 'Decomposing', 'Green', 'Deathly', 'Evil', 'Nasty', 'Brainless', 'Crazy', 'Horrible', 'Sloppy'];
+  var names = ['Bob', 'Katey', 'Harry', 'Pete', 'Dave', 'Sam', 'Eugene', 'Sally', 'Geoff', 'Nigel', 'Anton', 'Ben'];
+  var zombieName = adjectives[_.random(adjectives.length)] + ' ' + names[_.random(names.length)];
+  var pingTime = 2000;
+  var flushTime = 10000;
 
-	//	get local gateway IP & Zombie name
-	var interfaces = os.networkInterfaces();
-	var addresses = [];
-	for (k in interfaces) {
-	    for (k2 in interfaces[k]) {
-	        var address = interfaces[k][k2];
-	        if (address.family == 'IPv4' && !address.internal) {
-	            addresses.push(address.address)
-	        }
-	    }
-	}
 
-	var prowler = function() {
-		//	create socket
-		var server = dgram.createSocket('udp4');
+  //	get local gateway IP & Zombie name
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (k in interfaces) {
+    for (k2 in interfaces[k]) {
+      var address = interfaces[k][k2];
+      if (address.family == 'IPv4' && !address.internal) {
+	addresses.push(address.address)
+      }
+    }
+  }
 
-		//	message event
-		server.on("message", function (msg, rinfo) {
-			var bits = msg.toString().split(':');
+  var prowler = function() {
+    //	create socket
+    var server = dgram.createSocket('udp4');
 
-                  var found = _.find(ips, function(ip) {
+    //	message event
+    server.on("message", function (msg, rinfo) {
 
-                    return (ip.ip == bits[0]);
-                  });
+      try {
+        var info = JSON.parse(msg);
 
-                  if (!found) {
-                    console.log("Adding: '" + bits[0] + "'");
-                    ips.push({ip: bits[0], name: bits[1]});
-                  }
-		});
+        var found = _.find(ips, function(ip) {
+          return (ip.ip == info.ip);
+        });
 
-		//	start listening
-		server.on("listening", function () {
-			var address = server.address();
-		  console.log(zombieName + " is looking for brains on " + address.address + ":" + address.port);
-		});
+        if (!found) {
+          console.log("Adding: '" + info.ip + "'");
+          ips.push({ip: info.ip, name: info.name, present: 1});
+        } else {
+          found.present = 1;
+        }
 
-		server.bind(port);
+      } catch (err) {
+        return;
+      }
+    });
 
-		//	broadcast own IP to local network
-		function broadcast() {
-			setTimeout(broadcast, 10000);	
+    //	start listening
+    server.on("listening", function () {
+      var address = server.address();
+      console.log(zombieName + " is looking for brains on " + address.address + ":" + address.port);
+    });
 
-			var message = new Buffer(addresses[0] + ':' + zombieName);
-			server.setBroadcast(true);
-			server.send(message, 0, message.length, port, host);
-		}
-		broadcast();
-	}
+    server.bind(port);
 
-	//	create server and listen on 9876
-	var httpServer = function() {
-		http.createServer(function(request, response) {
+    //	broadcast own IP to local network
+    setTimeout(function() {
 
-			//	add request listner
-			request.on('end', function() {
+      console.log("Broadcast");
+      var info = {};
 
-				//	view setup
-				var view = {ips: ips};
-				var template = 'index.html';
+      info.name = zombieName;
+      info.ip = addresses[0];
+      info.resource = "resource here please";
 
-				//	write response headers
-				response.writeHead(200, {
-					'Content-Type': 'text/html'
-				});
+      var message = new Buffer(JSON.stringify(info));
 
-				//	send respose
-				fs.readFile(template, function(err, template) {
-					response.end(Mustache.to_html(template.toString(), view));
-				});
-			});
-		}).listen(9876);
+      server.setBroadcast(true);
+      server.send(message, 0, message.length, port, host);
 
-		console.log('Sssshhhh!, There are zombies on localhost:9876');
-	}
+    }, pingTime);
+
+    // Remove all remote clients that we haven't heard from.
+    setTimeout(function() {
+      _.each(ips, function(ip, i) {
+        if (!ip.present) {
+          ips[i] = null;
+        }
+      });
+
+    }, flushTime);
+  };
+
+  //	create server and listen on 9876
+  var httpServer = function() {
+    http.createServer(function(request, response) {
+
+      //	add request listner
+      request.on('end', function() {
+
+	//	view setup
+	var view = {ips: ips};
+	var template = 'index.html';
+
+	//	write response headers
+	response.writeHead(200, {
+	  'Content-Type': 'text/html'
+	});
+
+	//	send respose
+	fs.readFile(template, function(err, template) {
+	  response.end(Mustache.to_html(template.toString(), view));
+	});
+      });
+    }).listen(9876);
+
+    console.log('Sssshhhh!, There are zombies on localhost:9876');
+  }
 
   return {
-   serve: httpServer(),
-	 prowl: prowler(),
-	}
+    serve: httpServer(),
+    prowl: prowler(),
+  }
 })();
